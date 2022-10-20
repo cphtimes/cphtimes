1779c91ce64ab5547bae1268266cd332521570ca
Daniel Ran Lehmann <danielran11@gmail.com>
2022-10-20 14:36:30 +0200

    Worked a bit more on the article view.

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -209,6 +209,9 @@
           </div>
         </nav>
 
+        <div class="d-none alert alert-primary rounded-0 border-0" role="alert">
+          Live online, today at 9 AM CEST.
+        </div>
         <nav class="sticky-top navbar navbar-light bg-white shadow-sm">
           <div class="d-flex justify-content-between container-fluid">
             <div class="d-block d-lg-none">
@@ -342,9 +345,9 @@
             </div>
           </div>
         </nav>
-        <div class="container">
+        <div class="container pb-5 my-md-2 my-lg-3 my-xl-4">
           <div class="row">
-            <div class="col-lg-9 col-xl-8 pe-lg-4 pe-xl-0 pt-4">
+            <div class="col-lg-9 col-xl-8 pe-lg-4 pe-xl-0">
               <small class="text-uppercase mb-3">
                 <strong>{!! $article->article_section !!}</strong>
               </small>
@@ -357,9 +360,33 @@
                 <div class="d-flex align-items-center mb-4 me-4"><span class="fs-sm me-2">By:</span><a class="nav-link position-relative fw-semibold p-0" href="#author" data-scroll="" data-scroll-offset="80">{!! $article->author!!}<span class="d-block position-absolute start-0 bottom-0 w-100" style="background-color: currentColor; height: 1px;"></span></a> <span class="d-none"> at {!! $article->date_published !!}</span></div>
               </div>
               <figure class="figure w-100">
+                <div class="d-none ratio ratio-16x9">
+                  <iframe width="1920" height="813" src="https://www.youtube.com/embed/75A5_5Rk-dM" title="Hygge på recept - Hannah" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
                 <img class="figure-img" style="object-fit: cover;" height="100%" width="100%" src="{!! $article->thumbnail_url !!}"/>
                 <figcaption class="figure-caption">A caption for the above image.</figcaption>
               </figure>
+
+              <!-- Check if article should be readable out loud? bool -->
+              <div class="py-5 d-flex justify-content-start align-items-center">
+                <div class="me-3">
+                  <button id="article-listen-toggle-btn" style="width: 48px; height: 48px" class="btn rounded-circle btn-dark me-1">
+                    <svg style="vertical-align: text-bottom" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
+                      <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
+                    </svg>
+                  </button>
+                </div>
+                <div class="me-auto">
+                  <p class="mb-0">Listen to the article</p>
+                </div>
+                <div>
+                  <button id="article-listen-stop-btn" style="width: 48px; height: 48px" class="btn rounded-circle btn-outline-dark">
+                    <svg style="vertical-align: text-bottom" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                      <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                    </svg>
+                  </button>
+                </div>
+              </div>
 
               {!! $article->article_body !!}
 
@@ -420,17 +447,17 @@
               <!-- Sharing-->
               <h5 class="mb-4 serif fst-italic">Share this article:</h5>
               <div class="mt-n3 ms-n3 mb-lg-5 mb-4 pb-3 pb-lg-0">
-                <a class="btn btn-outline-secondary btn-icon btn-sm btn-instagram rounded-circle mt-3" href="#">
+                <a style="width: 34px; height: 34px" class="btn btn-outline-secondary btn-icon btn-sm btn-instagram rounded-circle mt-3" href="#">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
                     <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/>
                   </svg>
                 </a>
-                <a class="btn btn-outline-secondary btn-icon btn-sm btn-facebook rounded-circle mt-3 ms-3" href="#">
+                <a style="width: 34px; height: 34px" class="btn btn-outline-secondary btn-icon btn-sm btn-facebook rounded-circle mt-3 ms-3" href="#">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
                     <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
                   </svg>
                 </a>
-                <a class="btn btn-outline-secondary btn-icon btn-sm btn-telegram rounded-circle mt-3 ms-3" href="#">
+                <a style="width: 34px; height: 34px" class="btn btn-outline-secondary btn-icon btn-sm btn-telegram rounded-circle mt-3 ms-3" href="#">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telegram" viewBox="0 0 16 16">
                     <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.287 5.906c-.778.324-2.334.994-4.666 2.01-.378.15-.577.298-.595.442-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294.26.006.549-.1.868-.32 2.179-1.471 3.304-2.214 3.374-2.23.05-.012.12-.026.166.016.047.041.042.12.037.141-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8.154 8.154 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629.093.06.183.125.27.187.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.426 1.426 0 0 0-.013-.315.337.337 0 0 0-.114-.217.526.526 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09z"/>
                   </svg>
@@ -472,6 +499,58 @@
           </aside>
           </div>
         </div>
+
+        <!-- Comments -->
+        <section class="container pt-xl-2 pb-5 mb-md-2 mb-lg-4 mb-xl-5" id="comments">
+          <div class="border-top border-bottom">
+            <!-- Comments collapse-->
+            <div class="collapse" id="commentsCollapse" style="max-width: 54rem;">
+              <!-- Comment-->
+              <div class="border-bottom py-4 mt-2 mb-4">
+                <div class="d-flex align-items-center pb-1 mb-3"><img class="rounded-circle" src="assets/img/avatar/08.jpg" width="48" alt="Comment author">
+                  <div class="ps-3">
+                    <h6 class="mb-0">Albert Flores</h6><span class="fs-sm text-muted">5 hours ago</span>
+                  </div>
+                </div>
+                <p class="pb-2 mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tellus lectus, tempus eu urna eu, imperdiet dignissim augue. Aliquam fermentum est a ligula bibendum, ac gravida ipsum dictum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur suscipit quam ut velit condimentum, nec mollis risus semper. Curabitur quis mauris eget ligula tincidunt venenatis. Sed congue pulvinar hendrerit.</p>
+                <button class="btn btn-link fs-sm fw-semibold px-0 py-2" type="button">Reply<i class="ai-redo fs-xl ms-2"></i></button>
+              </div>
+              <!-- Comment-->
+              <div class="border-bottom pt-2 pb-4">
+                <div class="d-flex align-items-center pb-1 mb-3"><img class="rounded-circle" src="assets/img/avatar/11.jpg" width="48" alt="Comment author">
+                  <div class="ps-3">
+                    <h6 class="mb-0">Jenny Wilson</h6><span class="fs-sm text-muted">2 days ago at 9:20</span>
+                  </div>
+                </div>
+                <p class="pb-2 mb-0">Pellentesque urna pharetra, quis maecenas. Sit dolor amet nulla aenean eu, ac. Nisl mi tempus, iaculis viverra vestibulum scelerisque imperdiet montes mauris massa elit pretium elementum eget tortor quis</p>
+                <button class="btn btn-link fs-sm fw-semibold px-0 py-2" type="button">Reply<i class="ai-redo fs-xl ms-2"></i></button>
+                <!-- Comment reply-->
+                <div class="card card-body border-0 bg-secondary mt-4">
+                  <div class="d-flex align-items-center pb-1 mb-3"><img class="rounded-circle" src="assets/img/avatar/10.jpg" width="48" alt="Comment author">
+                    <div class="ps-3">
+                      <h6 class="mb-0">Ralph Edwards</h6><span class="fs-sm text-muted">2 days ago at 11:45</span>
+                    </div>
+                  </div>
+                  <p class="mb-0"><a class="fw-bold text-decoration-none" href="#">@Jenny Wilson</a> Massa morbi duis et ornare urna dictum vestibulum pulvinar nunc facilisis ornare id at at ut arcu integer tristique placerat non turpis nibh turpis ullamcorper est porttitor.</p>
+                </div>
+              </div>
+              <!-- Comment-->
+              <div class="pt-4 mt-2 mb-2">
+                <div class="d-flex align-items-center pb-1 mb-3"><img class="rounded-circle" src="assets/img/avatar/07.jpg" width="48" alt="Comment author">
+                  <div class="ps-3">
+                    <h6 class="mb-0">Esther Howard</h6><span class="fs-sm text-muted">May 19, 2022</span>
+                  </div>
+                </div>
+                <p class="pb-2 mb-0">Donec et sollicitudin tellus. Duis maximus, dui eget egestas mattis, purus ex tempor nulla, quis tempor sapien neque at nisl. Aliquam eu nisi ut nisl ultrices posuere. Praesent dignissim efficitur nisi, a hendrerit ipsum elementum sit amet. Vivamus non ante nisl. Nunc faucibus velit at eros mollis semper.</p>
+                <button class="btn btn-link fs-sm fw-semibold px-0 py-2" type="button">Reply<i class="ai-redo fs-xl ms-2"></i></button>
+              </div>
+            </div>
+            <!-- Comments toggle-->
+            <div class="nav">
+              <button class="btn btn-more w-100 justify-content-center p-3 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#commentsCollapse" aria-expanded="false" aria-controls="commentsCollapse" data-show-label="Show all comments" data-hide-label="Hide all comments"><span class="fw-normal opacity-70 ms-1">Show all comments (4)</span></button>
+            </div>
+          </div>
+        </section>
       </main>
 
       <!-- Subscription -->
@@ -535,6 +614,45 @@
 
 
       <!-- Optional JavaScript; choose one of the two! -->
+      <script>
+        let speech = new SpeechSynthesisUtterance();
+        speech.lang = "en";
+        speech.volume = 1.0;
+        speech.pitch = 1.0;
+        speech.rate = 1.0;
+        speech.text = "This is a point in the evolution of the planet that brings to the forefront of each individual’s thoughts the question of why me, why now and what is really going on in the reality that is right now in the time we are experiencing.";
+        let voices = [];
+        window.speechSynthesis.onvoiceschanged = () => {
+          voices = window.speechSynthesis.getVoices();
+          let voiceSelect = document.querySelector("#voices");
+          voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
+        };
+        const articleListenToggleBtn = document.getElementById('article-listen-toggle-btn');
+        const articleListenStopBtn = document.getElementById('article-listen-stop-btn');
+
+        const playIcon = '<svg style="vertical-align: text-bottom" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16"><path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/></svg>';
+        const pauseIcon = '<svg style="vertical-align: text-bottom" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pause-fill" viewBox="0 0 16 16"><path d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5zm5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5z"/></svg>';
+
+        articleListenToggleBtn.addEventListener("click", () => {
+          if (window.speechSynthesis.speaking == false) {
+            window.speechSynthesis.speak(speech);
+            articleListenToggleBtn.innerHTML = pauseIcon;
+          } else {
+            if (window.speechSynthesis.paused == false) {
+              articleListenToggleBtn.innerHTML = playIcon;
+              window.speechSynthesis.pause();
+            } else {
+              articleListenToggleBtn.innerHTML = pauseIcon;
+              window.speechSynthesis.resume();
+            }
+          }
+        });
+
+        articleListenStopBtn.addEventListener("click", () => {
+          articleListenToggleBtn.innerHTML = playIcon;
+          window.speechSynthesis.stop();
+        })
+      </script>
 
       <!-- Option 1: Bootstrap Bundle with Popper -->
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
