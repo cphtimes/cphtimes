e336134084acde77b0da58a73db40f14dbb04c8a
Daniel Ran Lehmann <danielran11@gmail.com>
2022-11-04 06:54:44 +0100

    Fixed up buttons and other layout things.

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -660,6 +660,140 @@
           </div>
         </div>
       </div>
+
+      <div class="pt-4 mt-md-5 pt-md-4 border-top border-2 container">
+        <footer class="pt-3">
+          <!-- <a style="font-family: 'UnifrakturMaguntia', cursive; font-size: 1.8rem;" class="navbar-brand" href="#">The Copenhagen Gates</a>-->
+          <div class="row px-md-3">
+            <div class="col-6 col-md-4 col-lg-2 px-4 px-md-0">
+              <h5 class="serif fst-italic">News</h5>
+              <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Homepage</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">World</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Health</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Media</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Technology</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Culture</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Business</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Politics</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Opinion</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Science</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">National</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Style</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Travel</a></li>
+              </ul>
+            </div>
+
+            <div class="col-6 col-md-4 col-lg-2 px-4 px-md-0">
+              <h5 class="serif fst-italic">Links</h5>
+              <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a target="_blank" href="https://home.solari.com/" class="nav-link p-0 text-muted">Solari Report</a></li>
+                <li class="nav-item mb-2"><a target="_blank" href="https://www.ukcolumn.org/" class="nav-link p-0 text-muted">UK Column</a></li>
+                <li class="nav-item mb-2"><a target="_blank" href="https://www.oraclefilms.com/" class="nav-link p-0 text-muted">Oracle Films</a></li>
+                <li class="nav-item mb-2"><a target="_blank" href="https://worldfreedomalliance.org/" class="nav-link p-0 text-muted">World Freedom Alliance</a></li>
+                <li class="nav-item mb-2"><a target="_blank" href="https://doctors4covidethics.org/" class="nav-link p-0 text-muted">Doctors for Covid Ethics</a></li>
+                <li class="nav-item mb-2"><a target="_blank" href="https://childrenshealthdefense.org/" class="nav-link p-0 text-muted">Childrens Health Defense</a></li>
+                <li class="nav-item mb-2"><a target="_blank" href="https://americasfrontlinedoctors.org/" class="nav-link p-0 text-muted">American Frontline Doctors</a></li>
+                <li class="nav-item mb-2"><a target="_blank" href="https://covid19criticalcare.com/" class="nav-link p-0 text-muted">FLCCC</a></li>
+                <li class="nav-item mb-2"><a target="_blank" href="https://www.stopworldcontrol.com/" class="nav-link p-0 text-muted">Stop World Control</a></li>
+                <li class="nav-item mb-2"><a target="_blank" href="https://perbraendgaard.dk/" class="nav-link p-0 text-muted">Per Brændgaard</a></li>
+                <li class="nav-item mb-2"><a target="_blank" href="https://vernoncoleman.org/" class="nav-link p-0 text-muted">Dr. Vernon Coleman</a></li>
+                <li class="nav-item mb-2"><a target="_blank" href="https://www.redvoicemedia.com/stew-peters-show/" class="nav-link p-0 text-muted">The Stew Peters Show</a></li>
+                <li class="nav-item mb-2"><a target="_blank" href="https://planetlockdownfilm.com/" class="nav-link p-0 text-muted">Planet Lockdown</a></li>
+                <li class="nav-item mb-2"><a target="_blank" href="https://thelightpaper.co.uk/" class="nav-link p-0 text-muted">The Light</a></li>
+              </ul>
+            </div>
+
+            <!--
+            <div class="col-2">
+              <h5>Communities</h5>
+              <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">World Freedom Alliance</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Doctors for Covid Ethics</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Childrens Health Defense</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">American Frontline Doctors</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FLCCC</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">StopWorldControl</a></li>
+              </ul>
+            </div>
+            -->
+
+            <div class="col-6 col-md-4 col-lg-2 px-4 px-md-0">
+              <h5 class="serif fst-italic">More</h5>
+              <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Tips</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Censorship</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Manage my account</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Funding</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+              </ul>
+            </div>
+
+            <div class="d-none d-lg-block col-12 col-md-8 col-lg-4 offset-0 offset-lg-1"> <!-- border-start ps-4 -->
+              <form action="https://cphgates.us20.list-manage.com/subscribe/post?u=e71c47511b5e02967960e85f4&amp;id=e1415b6230" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate="">
+                <h5 class="serif fst-italic">Subscribe to our newsletter</h5>
+                <p>Monthly digest of whats new and exciting from us.</p>
+                <div class="d-flex w-100 gap-2">
+                  <label for="newsletter1" class="visually-hidden">Email address</label>
+                  <input type="email" value="" name="EMAIL" class="required email form-control" id="mce-EMAIL" placeholder="Email address" required="">
+                  <button type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn btn-primary">Subscribe</button>
+                </div>
+              </form>
+
+              <!-- <div>
+                <iframe src="https://t.me/s/folkepartiet?embed=1"></iframe>
+              </div> -->
+            </div>
+          </div>
+
+          <div class="row d-none d-md-block px-md-3">
+            <div class="d-flex justify-content-between py-4 my-4 border-top px-0">
+              <p>© 2021 Copenhagen Gates. All rights reserved.</p>
+              <ul class="list-unstyled d-flex">
+                <li class="ms-3"><a class="nav-link" href="#">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-telegram" viewBox="0 0 16 16">
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.287 5.906c-.778.324-2.334.994-4.666 2.01-.378.15-.577.298-.595.442-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294.26.006.549-.1.868-.32 2.179-1.471 3.304-2.214 3.374-2.23.05-.012.12-.026.166.016.047.041.042.12.037.141-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8.154 8.154 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629.093.06.183.125.27.187.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.426 1.426 0 0 0-.013-.315.337.337 0 0 0-.114-.217.526.526 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09z"></path>
+                  </svg>
+                </a></li>
+                <li class="ms-3"><a class="nav-link" href="#">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+                    <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"></path>
+                  </svg>
+                </a></li>
+                <li class="ms-3"><a class="nav-link" href="#">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                    <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"></path>
+                  </svg>
+                </a></li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="row d-block d-md-none">
+            <div class="d-flex flex-column align-items-center justify-content-center py-4 mt-4 border-top px-4">
+              <p class="text-center">© 2021 Copenhagen Gates. All rights reserved.</p>
+              <ul class="list-unstyled d-flex">
+                <li class="ms-3"><a class="link-dark" href="#">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-telegram" viewBox="0 0 16 16">
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.287 5.906c-.778.324-2.334.994-4.666 2.01-.378.15-.577.298-.595.442-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294.26.006.549-.1.868-.32 2.179-1.471 3.304-2.214 3.374-2.23.05-.012.12-.026.166.016.047.041.042.12.037.141-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8.154 8.154 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629.093.06.183.125.27.187.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.426 1.426 0 0 0-.013-.315.337.337 0 0 0-.114-.217.526.526 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09z"></path>
+                  </svg>
+                </a></li>
+                <li class="ms-3"><a class="link-dark" href="#">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
+                    <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"></path>
+                  </svg>
+                </a></li>
+                <li class="ms-3"><a class="link-dark" href="#">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                    <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"></path>
+                  </svg>
+                </a></li>
+              </ul>
+            </div>
+          </div>
+        </footer>
+      </div>
     </div>
 
 
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -601,7 +601,7 @@
           <div class="row pb-5">
           <div class="col border-end">
             <a class="nav-link" href="{{ url(sprintf("/%s/%s/%s/%s/%s", date('Y', strtotime($articles[0]->date_published)), date('m', strtotime($articles[0]->date_published)), date('d', strtotime($articles[0]->date_published)), $articles[0]->article_section, $articles[0]->headline_dashed)) }}">
-              <article class="border-0 px-2">
+              <article class="card border-0 px-2">
                 <img style="object-fit: cover;" width="190px" height="190px" src="{!! $articles[0]->thumbnail_url !!}" alt="..." class="">
                 <div class="article-body card-body px-0 pb-0">
                   <p><small class="text-uppercase text-dark"><b>{!! $articles[0]->article_section !!}</b></small></p>
@@ -614,7 +614,7 @@
 
           <div class="col border-end">
             <a class="nav-link" href="{{ url(sprintf("/%s/%s/%s/%s/%s", date('Y', strtotime($articles[1]->date_published)), date('m', strtotime($articles[1]->date_published)), date('d', strtotime($articles[1]->date_published)), $articles[1]->article_section, $articles[1]->headline_dashed)) }}">
-              <article class="border-0 px-2">
+              <article class="card border-0 px-2">
                 <img style="object-fit: cover;" width="190px" height="190px" src="{!! $articles[1]->thumbnail_url !!}" alt="..." class="">
                 <div class="article-body card-body px-0 pb-0">
                   <p><small class="text-uppercase text-dark"><b>{!! $articles[1]->article_section !!}</b></small></p>
@@ -627,7 +627,7 @@
 
           <div class="col border-end">
             <a class="nav-link" href="{{ url(sprintf("/%s/%s/%s/%s/%s", date('Y', strtotime($articles[2]->date_published)), date('m', strtotime($articles[2]->date_published)), date('d', strtotime($articles[2]->date_published)), $articles[2]->article_section, $articles[2]->headline_dashed)) }}">
-              <article class="border-0 px-2">
+              <article class="card border-0 px-2">
                 <img style="object-fit: cover;" width="600px" height="330px" src="{!! $articles[2]->thumbnail_url !!}" alt="..." class="">
                 <div class="article-body card-body px-0 pb-0">
                   <p><small class="text-uppercase text-dark"><b>{!! $articles[2]->article_section !!}</b></small></p>
@@ -639,7 +639,7 @@
 
           <div class="col pe-0">
             <a class="nav-link" href="{{ url(sprintf("/%s/%s/%s/%s/%s", date('Y', strtotime($articles[3]->date_published)), date('m', strtotime($articles[3]->date_published)), date('d', strtotime($articles[3]->date_published)), $articles[3]->article_section, $articles[3]->headline_dashed)) }}">
-              <article class="border-0 px-2">
+              <article class="card border-0 px-2">
                 <img style="object-fit: cover;" width="100%" height="190px" src="{!! $articles[3]->thumbnail_url !!}" alt="..." class="">
                 <div class="article-body card-body px-0 pb-0">
                   <p><small class="text-uppercase text-dark"><b>{!! $articles[3]->article_section !!}</b></small></p>
@@ -711,60 +711,60 @@
           </div>
         </div>
 
-        <!-- deskop-->
-        <div class="d-none d-lg-block">
+        <!-- deskop :: d-none d-lg-block -->
+        <div class="">
           <div class="row pb-5">
-          <div class="col border-end">
-            <a class="nav-link" href="{{ url(sprintf("/%s/%s/%s/%s/%s", date('Y', strtotime($articles[4]->date_published)), date('m', strtotime($articles[4]->date_published)), date('d', strtotime($articles[4]->date_published)), $articles[4]->article_section, $articles[4]->headline_dashed)) }}">
-              <article class="border-0 px-2">
-                <img style="object-fit: cover;" width="600px" height="330px" src="{!! $articles[4]->thumbnail_url !!}" alt="..." class="">
-                <div class="article-body card-body px-0 pb-0">
-                  <p><small class="text-uppercase text-dark"><b>{!! $articles[4]->article_section !!}</b></small></p>
-                  <h5 class="article-title card-title fw-light crop-text-1">{!! $articles[4]->headline !!}</h5>
-                </div>
-              </article>
-            </a>
-          </div>
+            <div class="col border-end">
+              <a class="nav-link" href="{{ url(sprintf("/%s/%s/%s/%s/%s", date('Y', strtotime($articles[4]->date_published)), date('m', strtotime($articles[4]->date_published)), date('d', strtotime($articles[4]->date_published)), $articles[4]->article_section, $articles[4]->headline_dashed)) }}">
+                <article class="card border-0 px-2">
+                  <img style="object-fit: cover;" width="600px" height="330px" src="{!! $articles[4]->thumbnail_url !!}" alt="..." class="">
+                  <div class="article-body card-body px-0 pb-0">
+                    <p><small class="text-uppercase text-dark"><b>{!! $articles[4]->article_section !!}</b></small></p>
+                    <h5 class="article-title card-title fw-light crop-text-1">{!! $articles[4]->headline !!}</h5>
+                  </div>
+                </article>
+              </a>
+            </div>
 
-          <div class="col border-end">
-            <a class="nav-link" href="{{ url(sprintf("/%s/%s/%s/%s/%s", date('Y', strtotime($articles[5]->date_published)), date('m', strtotime($articles[5]->date_published)), date('d', strtotime($articles[5]->date_published)), $articles[5]->article_section, $articles[5]->headline_dashed)) }}">
-              <article class="border-0 px-2">
-                <img style="object-fit: cover;" width="190px" height="190px" src="{!! $articles[5]->thumbnail_url !!}" alt="..." class="">
-                <div class="article-body card-body px-0 pb-0">
-                  <p><small class="text-uppercase text-dark"><b>{!! $articles[5]->article_section !!}</b></small></p>
-                  <h5 class="article-title card-title fw-light crop-text-2">{!! $articles[5]->headline !!}</h5>
-                  <p class="opacity-50 crop-text-4">{!! $articles[5]->abstract !!}</p>
-                </div>
-              </article>
-            </a>
-          </div>
+            <div class="col border-end">
+              <a class="nav-link" href="{{ url(sprintf("/%s/%s/%s/%s/%s", date('Y', strtotime($articles[5]->date_published)), date('m', strtotime($articles[5]->date_published)), date('d', strtotime($articles[5]->date_published)), $articles[5]->article_section, $articles[5]->headline_dashed)) }}">
+                <article class="card border-0 px-2">
+                  <img style="object-fit: cover;" width="190px" height="190px" src="{!! $articles[5]->thumbnail_url !!}" alt="..." class="">
+                  <div class="article-body card-body px-0 pb-0">
+                    <p><small class="text-uppercase text-dark"><b>{!! $articles[5]->article_section !!}</b></small></p>
+                    <h5 class="article-title card-title fw-light crop-text-2">{!! $articles[5]->headline !!}</h5>
+                    <p class="opacity-50 crop-text-4">{!! $articles[5]->abstract !!}</p>
+                  </div>
+                </article>
+              </a>
+            </div>
 
-          <div class="col border-end">
-            <a class="nav-link" href="{{ url(sprintf("/%s/%s/%s/%s/%s", date('Y', strtotime($articles[6]->date_published)), date('m', strtotime($articles[6]->date_published)), date('d', strtotime($articles[6]->date_published)), $articles[6]->article_section, $articles[6]->headline_dashed)) }}">
-              <article class="border-0 px-2">
-                <img style="object-fit: cover;" width="100%" height="190px" src="{!! $articles[6]->thumbnail_url !!}" alt="..." class="">
-                <div class="article-body card-body px-0 pb-0">
-                  <p><small class="text-uppercase text-dark"><b>{!! $articles[6]->article_section !!}</b></small></p>
-                  <h5 class="article-title card-title fw-light crop-text-2">{!! $articles[6]->headline !!}</h5>
-                  <p class="opacity-50 crop-text-4">{!! $articles[6]->abstract !!}</p>
-                </div>
-              </article>
-            </a>
-          </div>
+            <div class="col border-end">
+              <a class="nav-link" href="{{ url(sprintf("/%s/%s/%s/%s/%s", date('Y', strtotime($articles[6]->date_published)), date('m', strtotime($articles[6]->date_published)), date('d', strtotime($articles[6]->date_published)), $articles[6]->article_section, $articles[6]->headline_dashed)) }}">
+                <article class="card border-0 px-2">
+                  <img style="object-fit: cover;" width="100%" height="190px" src="{!! $articles[6]->thumbnail_url !!}" alt="..." class="">
+                  <div class="article-body card-body px-0 pb-0">
+                    <p><small class="text-uppercase text-dark"><b>{!! $articles[6]->article_section !!}</b></small></p>
+                    <h5 class="article-title card-title fw-light crop-text-2">{!! $articles[6]->headline !!}</h5>
+                    <p class="opacity-50 crop-text-4">{!! $articles[6]->abstract !!}</p>
+                  </div>
+                </article>
+              </a>
+            </div>
 
-          <div class="col pe-0">
-            <a class="nav-link" href="{{ url(sprintf("/%s/%s/%s/%s/%s", date('Y', strtotime($articles[7]->date_published)), date('m', strtotime($articles[7]->date_published)), date('d', strtotime($articles[7]->date_published)), $articles[7]->article_section, $articles[7]->headline_dashed)) }}">
-              <article class="border-0 px-2">
-                <img style="object-fit: cover;" width="190px" height="190px" src="{!! $articles[7]->thumbnail_url !!}" alt="..." class="">
-                <div class="article-body card-body px-0 pb-0">
-                  <p><small class="text-uppercase text-dark"><b>{!! $articles[7]->article_section !!}</b></small></p>
-                  <h5 class="article-title card-title fw-light crop-text-2">{!! $articles[7]->headline !!}</h5>
-                  <p class="opacity-50 crop-text-4">{!! $articles[7]->abstract !!}</p>
-                </div>
-              </article>
-            </a>
+            <div class="col pe-0">
+              <a class="nav-link" href="{{ url(sprintf("/%s/%s/%s/%s/%s", date('Y', strtotime($articles[7]->date_published)), date('m', strtotime($articles[7]->date_published)), date('d', strtotime($articles[7]->date_published)), $articles[7]->article_section, $articles[7]->headline_dashed)) }}">
+                <article class="card border-0 px-2">
+                  <img style="object-fit: cover;" width="190px" height="190px" src="{!! $articles[7]->thumbnail_url !!}" alt="..." class="">
+                  <div class="article-body card-body px-0 pb-0">
+                    <p><small class="text-uppercase text-dark"><b>{!! $articles[7]->article_section !!}</b></small></p>
+                    <h5 class="article-title card-title fw-light crop-text-2">{!! $articles[7]->headline !!}</h5>
+                    <p class="opacity-50 crop-text-4">{!! $articles[7]->abstract !!}</p>
+                  </div>
+                </article>
+              </a>
+            </div>
           </div>
-        </div>
         </div>
         @endif
 
@@ -1095,17 +1095,17 @@
           <div class="d-flex justify-content-between py-4 my-4 border-top px-0">
             <p>© 2021 Copenhagen Gates. All rights reserved.</p>
             <ul class="list-unstyled d-flex">
-              <li class="ms-3"><a class="link-dark" href="#">
+              <li class="ms-3"><a class="nav-link" href="#">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-telegram" viewBox="0 0 16 16">
                   <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.287 5.906c-.778.324-2.334.994-4.666 2.01-.378.15-.577.298-.595.442-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294.26.006.549-.1.868-.32 2.179-1.471 3.304-2.214 3.374-2.23.05-.012.12-.026.166.016.047.041.042.12.037.141-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8.154 8.154 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629.093.06.183.125.27.187.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.426 1.426 0 0 0-.013-.315.337.337 0 0 0-.114-.217.526.526 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09z"/>
                 </svg>
               </a></li>
-              <li class="ms-3"><a class="link-dark" href="#">
+              <li class="ms-3"><a class="nav-link" href="#">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
                   <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
                 </svg>
               </a></li>
-              <li class="ms-3"><a class="link-dark" href="#">
+              <li class="ms-3"><a class="nav-link" href="#">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
                   <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
                 </svg>
