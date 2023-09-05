f48ff893fd945bd4f39794f41a467b57ae305a68
DanielRanLehmann <danielran11@gmail.com>
2023-09-05 06:48:21 +0200

    Published localized config file.

--- routes/web.php
+++ routes/web.php
@@ -44,20 +44,6 @@ Route::get('/{locale}', function ($locale) {
 
 Route::get('/', [HomepageController::class, 'show']);
 
-/*
-Route::group(['domain' => env('DOMAIN_DANISH', 'kbhporte.dk')], function() {
-    Route::get('/sektion/{section}', [SectionController::class, 'show'])->name('section');
-    Route::get('/sektion/{section}/{article}', [ArticleController::class, 'show'])->name('article');
-    Route::match(['get', 'post'], '/af/{username}', [AuthorController::class, 'show'])->name('author');
-});
-
-Route::group(['domain' => env('DOMAIN_ENGLISH', 'cphgates.com')], function() {
-    Route::get('/section/{section}', [SectionController::class, 'show'])->name('section');
-    Route::get('/section/{section}/{article}', [ArticleController::class, 'show'])->name('article');
-    Route::match(['get', 'post'], '/by/{username}', [AuthorController::class, 'show'])->name('author');
-});
-*/
-
 Route::localized(function () {
     Route::get(Lang::uri('section/{section}'), [SectionController::class, 'show'])->name('section');
     Route::get(Lang::uri('/section/{section}/{article}'), [ArticleController::class, 'show'])->name('article');
